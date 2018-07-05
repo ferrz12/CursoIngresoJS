babe3e47df0605521e7414b99b88cfa29fa9aaa4
ferrz12 <ferna.rz12@gmail.com>
2018-07-05 11:24:51 -0300

    Tp 1 terminado

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,49 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    var ancho, largo, cantDeAlambre;
+
+    largo = document.getElementById("Largo").value;
+    ancho = document.getElementById("Ancho").value;
+    
+    ancho = parseInt(ancho);
+    largo = parseInt(largo);
+
+    cantDeAlambre = ((ancho * 2) + (largo * 2)) * 3;
+
+    alert("La cantidad de alambre necesario es: " + cantDeAlambre);
 
 }
+
 function Circulo () 
 {
-	
+	 var radio, perimetro, cantDeAlambre;
+
+    radio = document.getElementById("Radio").value;
+    
+    radio = parseInt(radio);
+
+    perimetro = 2 * Math.PI * radio;
+
+    cantDeAlambre = radio * 3;
+
+    alert("La cantidad de alambre necesario es: " + cantDeAlambre);
 }
+
 function Materiales () 
+
 {
-	
+	var ancho, largo, cantDeCemento, cantDeCal;
+
+    largo = document.getElementById("Largo").value;
+    ancho = document.getElementById("Ancho").value;
+    
+    ancho = parseInt(ancho);
+    largo = parseInt(largo);
+
+    cantDeCemento = (ancho * largo) * 2;
+
+    cantDeCal = (ancho * largo) * 3;
+
+    alert("La cantidad de cemento es: " + cantDeCemento + " y la cantidad de bolsas de cal es: " + cantDeCal);
 }
