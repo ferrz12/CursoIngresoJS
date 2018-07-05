ca6913595eb2728f8bd7351ff62b569ff2c8a133
ferrz12 <ferna.rz12@gmail.com>
2018-07-05 10:06:43 -0300

    Clase2

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,17 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe;
+    var resultado;
+    var descuento;
+
+    importe = document.getElementById("importe").value;
+
+    importe = parseInt(importe);
+
+    descuento = importe * 0.25;
+
+    resultado = importe - descuento;
+
+    document.getElementById("resultado").value = resultado;
 }
