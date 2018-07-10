d182ef11b33d22950e341d25ebc43478f47bef10
ferrz12 <ferna.rz12@gmail.com>
2018-07-10 09:27:28 -0300

    ejercicios de recuperatorios de parciales, los primeros 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
+    var precio, descuento, IVA, calculoIVA, precioFinal;
+
+    precio = prompt("Ingrese precio");
+    descuento = prompt("Ingrese descuento");
+
+    precio = parseInt(precio);
+    descuento = parseInt(descuento);
+
+    precio = precio - descuento; 
+
+    IVA = 0.21;
+
+    alert("El descuento en dinero es de: " + descuento + ", el precio con descuento es de: " + precio + " y el IVA es de: " + IVA);
+    
+    calculoIVA = precio * IVA;
+
+    precioFinal = precio + calculoIVA; 
+
+    document.getElementById("elPrecioFinal").value = precioFinal;
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,20 @@
 
 function mostrar()
 {
+    var base;
+    var altura;
+    var area;
+    var perimetro;
 
+    base = prompt("Ingrese base");
+    altura = prompt("Ingrese altura");
+
+    base = parseInt(base);
+    altura = parseInt(altura);
+
+    area = (base*altura)/2
+
+    perimetro = 3 * base;
+
+    alert("El area es: " + area + " y el perimetro es: " + base);
 }
