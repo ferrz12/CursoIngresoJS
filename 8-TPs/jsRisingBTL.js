1a73be80aa935678ebdb80e97467f146795aba9d
ferrz12 <polaris12>
2018-07-26 12:19:49 -0300

    tp 12 hecho

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,100 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
+ var edad;
+ var sexo;
+ var estadoCivil;
+ var sueldo;
+ var legajo;
+ var nacionalidad;
+ var validar = true;
+
  
+ while(validar){
+
+     edad = prompt("Ingrese edad");
+     
+     edad = parseInt(edad);
+
+     if(edad >= 18 && edad <= 90){
+        document.getElementById("Edad").value = edad
+            }else{
+                alert("La edad tiene que ser entre 18 y 90");
+                break;  
+            }
+
+     sexo = prompt("Ingrese sexo: “M” para masculino o “F” para femenino");       
+
+     if(sexo == "M" || sexo == "F"){
+        document.getElementById("Sexo").value = sexo;
+            }else{
+                alert("Ingrese sexo entre F o M");
+                break;
+            }
+
+    estadoCivil = prompt("Ingrese estado civil: 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos");        
+
+    estadoCivil = parseInt(estadoCivil);
+
+    switch (estadoCivil) {
+        case 1:
+            document.getElementById("EstadoCivil").value = "Soltero";                         
+            break;
+        case 2:
+            document.getElementById("EstadoCivil").value = "Casado";                         
+            break;
+        case 3:
+            document.getElementById("EstadoCivil").value = "Divorciado";                         
+            break;
+        case 4:
+            document.getElementById("EstadoCivil").value = "Viudo";                         
+            break;                 
+        default:
+            alert("Ingrese un estado civil correcto");
+            break;
+    }
+
+    sueldo = prompt("Ingrese sueldo bruto");
+
+    sueldo = parseInt(sueldo);
+
+    if(sueldo >= 8000){
+        document.getElementById("Sueldo").value = sueldo;
+    }else{
+        alert("Ingrese un saldo no menor a 8000");
+    }
+
+    legajo = prompt("Ingrese numero de legajo");
+
+    legajo = parseInt(legajo);
+
+    if(legajo >= 0000 && legajo <= 9999){
+        document.getElementById("Legajo").value = legajo;
+        }else{
+            alert("Ingrese un legajo de 4 cifras");
+            break;
+        }
+
+    nacionalidad = prompt("Ingrese nacionalidad: “A” para argentinos, “E” para extranjeros, “N” para nacionalizados");
+
+    switch (nacionalidad) {
+        case "A":
+            document.getElementById("Nacionalidad").value = "Argentino";
+            break;
+        case "E":
+            document.getElementById("Nacionalidad").value = "Extranjero";
+            break;
+        case "N":
+            document.getElementById("Nacionalidad").value = "Nacionalizado";
+            break;
+           
+        default:
+            break;
+    }
+
+break;                 
+
+ }
+
+
 }
