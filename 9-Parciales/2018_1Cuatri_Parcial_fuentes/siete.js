ee7631403c908a25d7d3d65e14afa747af79e79a
ferrz12 <polaris12>
2018-07-31 10:32:01 -0300

    ejercicio 7 parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,91 @@
 function mostrar()
 {
+    var nombre;
+    var sexo;
+    var edad;
+    var cont = 0;
+    var contHombres = 0;
+    var contMujeres = 0;
+    var contMayores = 0;
+    var contMenores = 0;
+    var contHombresMayores = 0;
+    var min = 0;
+    var max = 101;
+    var promedioMujeres = 0;
+    var promedioHombres = 0;
+    var promedioTotal = 0;
+    var nombreViejo;
+    var nombreJoven;
+    var sexoViejo;
+    var nombreMujerVieja;
+
+    while(cont < 4){
+
+        cont++;
+
+        nombre = prompt("Ingrese nombre");
+
+        sexo = prompt("Ingrese sexo");
+        sexo = sexo.toUpperCase();
+        while(sexo != "M" && sexo != "F"){
+            sexo = prompt("Ingrese una sexo valido");
+        }
+
+        edad = prompt("Ingrese edad");
+        edad = parseInt(edad);
+        while((edad <= 0 || edad >100) || isNaN(edad)){
+            edad = prompt("Ingrese una edad valida");
+            edad = parseInt(edad);
+        }
+
+        if(sexo == "M"){
+            contHombres++;
+            promedioHombres = promedioHombres + edad;
+        }
+        
+        if(sexo == "F"){
+            contMujeres++;
+            promedioMujeres = promedioMujeres + edad;
+        }
+
+        if(edad >= 18){
+            contMayores++;
+        }else{
+            contMenores++;
+        }
+
+        if(sexo == "M" && edad >= 18){
+            contHombresMayores++;
+        }
+
+        if(edad > max){
+            max = edad
+            nombreViejo = nombre;
+        }
+        
+        if(edad<min){
+            min = edad;
+
+        }
+
+
+
+    }
+
+promedioMujeres = promedioMujeres/contMujeres;
+promedioHombres = promedioHombres/contHombres;
+promedioTotal = promedioHombres + promedioMujeres;
+
+ document.write("Cantidad de hombres = " + contHombres + "<br>");
+ document.write("Cantidad de mujeres = " + contMujeres + "<br>");
+ document.write("Cantidad de mayores = " + contMayores + "<br>");
+ document.write("Cantidad de menores = " + contMenores + "<br>");
+ document.write("Cantidad de hombres mayores = " + contHombresMayores + "<br>");
+ document.write("La edad mas baja = " + min + "<br>");
+ document.write("La edad mas alta = " + max + "<br>");
+ document.write("El promedio de edad de hombres es = " + promedioHombres + "<br>");
+ document.write("El promedio de edad de mujeres es = " + promedioMujeres + "<br>");
+ document.write("El promedio de edad de hombres es = " + promedioTotal + "<br>");
+ document.write("El nombre del mas viejo es = " + nombreViejo + "<br>");
 
 }
